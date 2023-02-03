9051615bbb86b0fb50874b33efe50246e4d6b3f6
Curtis Wong <cwong990@berkeley.edu>
2023-02-03 15:00:55 -0800

    more lc feb3

--- arrays/median_of_two_sorted_arrays.py
+++ arrays/median_of_two_sorted_arrays.py
@@ -5,24 +5,34 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        # find median of nums1
-        m1 = (len(nums1)) // 2
-        if len(nums1) % 2 == 0:
-            m1_ = m1 - 1
-            median1 = (nums1[m1] + nums1[m1_]) / 2
-        else:
-            median1 = nums1[m1]
-        
-        # find median of nums2
-        m2 = (len(nums2)) // 2
-        if len(nums2) % 2 == 0:
-            m2_ = m2 - 1
-            median2 = (nums2[m2] + nums2[m2_]) / 2
-        else:
-            median2 = nums2[m2]
-        print(float(median1), float(median2))
-        print((median1 + median2)/2)
-        return (median1 + median2) / 2
+        # [1,2,3,4,5,6,7,8]
+        # [1,2,3,4,5]
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
+        if len(B) < len(A):
+            A, B = B, A
+        # only perform binary search on smaller sorted array
+        l, r = 0, len(A) - 1
+        while True:
+            # find midpoint for sorted arrays
+            i = (l+r) // 2
+            j = half - i - 2
+
+            # find rightmost of left partitions and leftmost of right partitions
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i + 1] if (i+1) < len(A) else float("infinity")
+            Bleft = B[j] if j >= 0 else float("-infinity")
+            Bright = B[j + 1] if (j+1) < len(B) else float("infinity")
+            # odd and even partition
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2:
+                    return min(Aright, Bright)
+                return (max(Aleft, Bleft)*1.0 + min(Aright, Bright)*1.0) / 2
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1
 
 if __name__ == "__main__":
     x = [1,2]
